73e7e444674dd5de3ffe20ee06ceaeb0a1f4d80e
Mike D'Antonio <mikedantonio56@gmail.com>
2017-06-25 20:03:53 +0000

    About me

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,8 @@
 class StudentsController < ApplicationController
-
+    def mike
+        @name = "Mike"
+        @bio = "I live in Orland Park. I attend Mount Carmel Highschool and I am going into my sophomore year. I plan to attend Stanford and study computer science. "
+        @restaurant = "Ruth's Chris"
+        @show = "The Secret Lives of the SUper Rich"
+    end     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 # Follow this format for your route: get '/harsha', to: 'students#harsha'
+    get '/mike', to: 'students#mike'
 end
